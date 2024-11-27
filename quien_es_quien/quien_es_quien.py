9edff6a336c60b527e032257c345c2b0667d8fcf
Kaneki <alex123viri@gmail.com>
2024-11-27 20:01:19 +0100

    style(quien_es_quien.py): :lipstick: Cambiar estilo placeholder.
    Hemos cambiado los estilos del placeholder, porque el grid anteriormente usado no causó algunos problemas. Hemos hecho 4 modulos (bloque_cartas(), menu(), caja_input() y personaje_a_adviniar()) con sus ajustes y estilos para tener una idea de la colocacción de cada bloque.

--- quien_es_quien/quien_es_quien.py
+++ quien_es_quien/quien_es_quien.py
@@ -1,31 +1,61 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
 from rxconfig import config
 from quien_es_quien import style
 
+def bloque_cartas() -> rx.Component:
+    return rx.center(
+            rx.box(
+            width="50%",
+            height="75vh",
+            background_color="gray",
+            position="absolute",
+            left="25%",
+            top="0",
+            margin="0",
+            align = 'center'
+    )
+    )
 
-class State(rx.State):
-    """The app state."""
+def menu() -> rx.Component:
+    return rx.box(
+        width="15%",
+        height="60vh",
+        background_color="pink",
+        position="absolute",
+        left="0%",
+        top="0",
+        margin="2rem",
+    )
 
-    ...
+def caja_input() -> rx.Component:
+    return rx.box(
+        rx.input(placeholder="Ej: ¿Lleva gafas?", width="100%"),
+        width="50%",
+        position="absolute",
+        left="25%",
+        top="80vh",  
+        margin="1rem",
+    )
 
-
-def cartas() -> rx.Component:
-    return rx.grid(
-        rx.foreach(
-            rx.Var.range(24),
-            lambda i: rx.card(f"Card {i + 1}", height="20vh"),
-        ),
-        columns="8",
-        spacing="3",
-        width="60%",
+def personaje_a_adivinar() -> rx.Component:
+    return rx.box(
+        "Pedro",
+        width="15%",
+        height="35vh",
+        background_color="red",
+        position="absolute",
+        left="80%",
+        top="18%",
+        margin="2rem",
     )
 
 def index() -> rx.Component:
-    return rx.center(
-        cartas()
-         
+    return rx.box(
+        menu(),
+        bloque_cartas(),
+        personaje_a_adivinar(),
+        caja_input(),
     )
 
 app = rx.App()
