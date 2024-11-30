fe1129169c5445d675c3c68b6e1d948b0f7f9957
Pablo <pablogonzaleztemes@gmail.com>
2024-11-30 20:08:28 +0100

    feat(FRONTEND): :sparkles: Mostrar imagen en cartas
    Se añadió la funcionalidad de mostrar una imagen en todas las cartas.

--- quien_es_quien/quien_es_quien.py
+++ quien_es_quien/quien_es_quien.py
@@ -4,23 +4,33 @@ from rxconfig import config
 from quien_es_quien import style
 from quien_es_quien import state
 
-def bloque_cartas() -> rx.Component:
+
+def zona_de_personajes() -> rx.Component:
     return rx.center(
-        rx.box(
-            style = style.caja_cartas,
+        rx.grid(
+            rx.foreach(
+                rx.Var.range(24), lambda i:
+                    rx.card(
+                        rx.image(
+                            src="Alfred.jpg", width="100px", height="auto"
+                        ),
+                        height="13em",
+                        width="7.5em"
+                ),
+            ),
+            columns="8",
+            spacing="4",
+            width="60%",
+        )
     )
-    )
-    
-def menu() -> rx.Component:
-    return rx.box(
-        style = style.menu,
-    )
-    
-def caja_texto() -> rx.Component:
-    return rx.box(
+
+def barra_de_accion() -> rx.Component:
+    return rx.hstack(
+
         rx.input(placeholder="Ej: ¿Lleva gafas?", width="75%",),
         rx.button("Enviar"),
         style=style.caja_texto,
+        left="28%",
     )
 
     
@@ -29,13 +39,12 @@ def personaje_a_adivinar() -> rx.Component:
         state.escoger_carta(),
         style = style.personaje_misterioso
     )
-    
+
 def index() -> rx.Component:
     return rx.box(
-        menu(),
-        bloque_cartas(),
+        zona_de_personajes(),
         personaje_a_adivinar(),
-        caja_texto(),
+        barra_de_accion(),
     )
 
 app = rx.App()
