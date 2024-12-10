e8131ce1bc7903845e31f362a9eae38eae4c3c6c
Pablo González <pablogonzaleztemes@gmail.com>
2024-12-10 11:22:46 +0100

    refactor(FRONT-BACKEND): :recycle: refactorizar nombres variables y funciones
    Se cambiaron los nombres de ciertas variables para representar mejor su función.

--- quien_es_quien/state.py
+++ quien_es_quien/state.py
@@ -6,8 +6,8 @@ import unicodedata
 
 class State(rx.State):
 
-    pregunta_usuario: str = ""
-    pregunta_limpia: str = ""
+    mensaje: str = ""
+    mensaje_limpio: str = ""
     show: list = [True] * 24
     cartas_tapadas: list = []
     cartas_a_tapar: list = []
@@ -15,58 +15,58 @@ class State(rx.State):
     elegido = ""
     elegido_num = ""
 
-    def escoger_carta(self):
+    def escoger_personaje(self):
         self.elegido = random.choice(personajes.integrantes)
         self.elegido_num = personajes.integrantes.index(self.elegido) + 1
     
-    def change(self):
+    def girar_carta(self):
         for carta in self.cartas_tapadas:
             self.show[carta] = False
 
 
-    def escribir_pregunta_usuario(self, value):
-        self.pregunta_usuario = value
+    def detectar_mensaje(self, value):
+        self.mensaje = value
 
 
-    def limpiar_palabra(self, texto):
+    def limpiador_de_mensaje(self, texto):
 
         texto_limpio = ''.join(char for char in unicodedata.normalize('NFD', texto) if not unicodedata.combining(char)).lower()
         return texto_limpio
 
 
-    def mensaje_usuario(self):
-        if self.pregunta_usuario:
-            self.pregunta_limpia = self.limpiar_palabra(self.pregunta_usuario)
-            self.pregunta_usuario = ""
-            self.identificar_caracteristicas()
+    def limpiar_mensaje(self):
+        if self.mensaje:
+            self.mensaje_limpio = self.limpiador_de_mensaje(self.mensaje)
+            self.mensaje = ""
+            self.analizar_mensaje()
 
 
     def adivinar(self):
         for persona in personajes.integrantes:
-            if self.pregunta_limpia in persona.nombre:
-                if self.pregunta_limpia == self.elegido.nombre:
+            if self.mensaje_limpio in persona.nombre:
+                if self.mensaje_limpio == self.elegido.nombre:
                     self.fin_de_juego = True
                     return rx.toast.success("¡Has ganado!")
-                elif self.pregunta_limpia != self.elegido.nombre:
+                elif self.mensaje_limpio != self.elegido.nombre:
                     self.fin_de_juego = True
                     return rx.toast.success("Das pena XD")
 
 
-    def identificar_caracteristicas(self):
-        if self.pregunta_limpia == "reset":
+    def analizar_mensaje(self):
+        if self.mensaje_limpio == "reset":
             self.show = [True] * 24
             self.cartas_tapadas = []
             self.fin_de_juego = False
-            self.escoger_carta()
+            self.escoger_personaje()
         for persona in personajes.integrantes:
-            if self.pregunta_limpia in str(self.elegido):
-                if self.pregunta_limpia not in str(persona) and personajes.integrantes.index(persona) not in self.cartas_tapadas:
+            if self.mensaje_limpio in str(self.elegido):
+                if self.mensaje_limpio not in str(persona) and personajes.integrantes.index(persona) not in self.cartas_tapadas:
                     self.cartas_tapadas.append(personajes.integrantes.index(persona))
             else:
-                if self.pregunta_limpia in str(persona) and personajes.integrantes.index(persona) not in self.cartas_tapadas:
+                if self.mensaje_limpio in str(persona) and personajes.integrantes.index(persona) not in self.cartas_tapadas:
                     self.cartas_tapadas.append(personajes.integrantes.index(persona))
-        self.change()
+        self.girar_carta()
 
     def test(self):
-        self.mensaje_usuario()
+        self.limpiar_mensaje()
         self.adivinar()
