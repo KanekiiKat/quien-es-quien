9286b860aeabe621e61c75ce62665ba7efd746ea
KanekiiKat <alex123viri@gmail.com>
2024-12-11 13:30:31 +0100

    feat(BACKEND): :sparkles: Mejorar limpiar mensaje.
    Hemos añadido un isalpha en la función limpiar_mensaje paraa que si pones el mensaje con simbolos te lo detecte igualmente.

--- quien_es_quien/backend/limpiar_mensaje.py
+++ quien_es_quien/backend/limpiar_mensaje.py
@@ -2,6 +2,6 @@ import unicodedata
 
 def limpiar_mensaje(state):
     if state.mensaje:
-        state.mensaje_limpio = ''.join(char for char in unicodedata.normalize('NFD', state.mensaje) if not unicodedata.combining(char)).lower()
+        state.mensaje_limpio = ''.join(char for char in unicodedata.normalize('NFD', state.mensaje) if not unicodedata.combining(char) and char.isalpha()).lower()
         state.mensaje = ""
         state.analizar_mensaje()
