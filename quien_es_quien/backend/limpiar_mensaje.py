4597e7607121465d90c4f3ab53b3d715c2be47a4
Pablo González <pablogonzaleztemes@gmail.com>
2024-12-12 09:53:55 +0100

    refactor(BACKEND): :recycle: Refactorizar caracteres en inglés
    Se refactorizó una variable para que coincida con el idioma del resto del proyecto.

--- quien_es_quien/backend/limpiar_mensaje.py
+++ quien_es_quien/backend/limpiar_mensaje.py
@@ -2,6 +2,6 @@ import unicodedata
 
 def limpiar_mensaje(state):
     if state.mensaje:
-        state.mensaje_limpio = ''.join(char for char in unicodedata.normalize('NFD', state.mensaje) if not unicodedata.combining(char) and char.isalpha()).lower()
+        state.mensaje_limpio = ''.join(caracter for caracter in unicodedata.normalize('NFD', state.mensaje) if not unicodedata.combining(caracter) and caracter.isalpha()).lower()
         state.mensaje = ""
         state.analizar_mensaje()
